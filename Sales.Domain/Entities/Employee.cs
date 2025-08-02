24873ef1e4d51421dff95460b672189bb807f386
fernando-ibz <fernando.ibz.g@gmail.com>
2025-08-02 02:39:55 -0500

    Fix nullable properties

--- Sales.Domain/Entities/Employee.cs
+++ Sales.Domain/Entities/Employee.cs
@@ -11,8 +11,8 @@ namespace Sales.Domain.Entities
         public DateTime HireDate { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
-        public string Region { get; set; }
-        public string PostalCode { get; set; }
+        public string? Region { get; set; }
+        public string? PostalCode { get; set; }
         public string Country { get; set; }
         public string Phone { get; set; }
     }
